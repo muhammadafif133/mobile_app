5bdda252b90233001c56cc7018bc6a2f6ba99f96
binzainudm <binzainudm@coventry.ac.uk>
2021-04-05 20:05:52 +0100

    Sign up and log in commit

---
+++ app/src/main/java/com/example/fitnessandworkout/LoginActivity.java
@@ -0,0 +1,49 @@
+package com.example.fitnessandworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userEmail, password;
+    Button login;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userEmail = (EditText)findViewById(R.id.etEmail);
+        password = (EditText)findViewById(R.id.etPassword);
+        login = (Button)findViewById(R.id.btnLogIn);
+        DB = new DBHelper(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String  email = userEmail.getText().toString();
+                String  pass = password.getText().toString();
+
+                if(email.equals("") || pass.equals(""))
+                    Toast.makeText(LoginActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkemailpassword = DB.checkEmailPassword(email, pass);
+                    if(checkemailpassword == true) {
+                        Toast.makeText(LoginActivity.this, "Log in successfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/fitnessandworkout/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.fitnessandworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText userEmail, password;
+    Button signUp, signIn;
+    TextView info;
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userEmail = (EditText)findViewById(R.id.etEmail);
+        password = (EditText)findViewById(R.id.etPassword);
+        info = (TextView)findViewById(R.id.tvInfo);
+        signUp = (Button)findViewById(R.id.btnSignUp);
+        signIn = (Button)findViewById(R.id.btnSignIn);
+        DB = new DBHelper(this);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String  email = userEmail.getText().toString();
+                String  pass = password.getText().toString();
+
+                if(email.equals("") || pass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkemail = DB.checkEmail(email);
+                    if(checkemail == false){
+                        Boolean insert = DB.insertData(email, pass);
+                        if(insert == true) {
+                            Toast.makeText(MainActivity.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        }else{
+                            Toast.makeText(MainActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this, "Email already exist. Please use different email", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+        });
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
