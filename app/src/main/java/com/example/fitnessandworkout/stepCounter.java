dac090a166f76138d56c10dbb87cf00103ca9186
binzainudm <binzainudm@coventry.ac.uk>
2021-04-11 02:53:17 +0100

    setting up sensor

--- app/src/main/java/com/example/fitnessandworkout/stepCounter.java
+++ app/src/main/java/com/example/fitnessandworkout/stepCounter.java
@@ -2,13 +2,53 @@ package com.example.fitnessandworkout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class stepCounter extends AppCompatActivity {
+
+    private TextView tvStepDetector;
+    private double MagnitudePrevious = 0;
+    private Integer stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_counter);
+
+        tvStepDetector = findViewById(R.id.tvStepDetector);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if (sensorEvent != null ){
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
+
+                    double Magnitude = Math.sqrt(x_acceleration * x_acceleration + y_acceleration * y_acceleration + z_acceleration * z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if(MagnitudeDelta > 6){
+                        stepCount++;
+                        tvStepDetector.setText(stepCount.toString());
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
