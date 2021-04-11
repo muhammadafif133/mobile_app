e201f608b2c8dda97b834f9c4422590aa7291b82
binzainudm <binzainudm@coventry.ac.uk>
2021-04-11 01:51:48 +0100

    step counter first commit

---
+++ app/src/main/java/com/example/fitnessandworkout/stepCounter.java
@@ -0,0 +1,14 @@
+package com.example.fitnessandworkout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class stepCounter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_counter);
+    }
+}
